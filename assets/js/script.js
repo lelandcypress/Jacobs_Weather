c1cbf3427c6f3eb830af59236f34073e979f73a9
lelandcypress <80779467+lelandcypress@users.noreply.github.com>
2021-06-12 10:49:55 -0400

    Search History Rendering on Application, need to add event listeners and classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var stateSearch = document.getElementById("state-search-input");
 var searchBTN = document.getElementById("searchBtn");
 var displayCity = document.getElementById("display-city");
 var displayDate = document.getElementById("current-date");
+var displayFailure = document.getElementById("failure");
+var displayExample = document.getElementById("example");
 var currentWeather = document.getElementById("display-weather");
 var currentTemp = document.getElementById("current-temp");
 var currentWind = document.getElementById("current-wind");
@@ -10,29 +12,37 @@ var currentHumidity = document.getElementById("current-humidity");
 var currentUV = document.getElementById("current-uv");
 var mainIcon = document.getElementById("current-weather-icon");
 var currentDate = document.getElementById("current-date");
+var displayRecentSearch = document.getElementById("display-recent-search");
+var searchItems = [];
 
+currentDate.textContent = " " + moment().format("dddd, MMMM Do");
 searchBTN.addEventListener("click", function () {
   userCity = citySearch.value.trim();
-  userState = stateSearch.value.trim();
-  getLocation(userCity, userState);
+  getLocation(userCity);
+  updateLocalStorage(userCity);
+  renderPreviousSearch();
 });
 
-function getLocation(city, state) {
+function getLocation(city) {
   var getUrl =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
-    "," +
-    state +
-    ",us&appid=11c33b04286bccb73c3817509d58931c";
+    ",,us&appid=11c33b04286bccb73c3817509d58931c";
   fetch(getUrl)
     .then(function (response) {
-      return response.json();
+      if (response == "[]") {
+        displayFailure.textContent = "Please enter a valid city, state";
+        displayExample.textContent = "ex: Dallas, TX";
+      } else {
+        return response.json();
+      }
     })
     .then(function (data) {
       var lat = data[0].lat;
       var long = data[0].lon;
+      var searchName = data[0].name + ", " + data[0].state;
       getWeather(lat, long);
-      displayCity.textContent = city + ", " + state;
+      displayCity.textContent = searchName;
     });
 }
 
@@ -50,7 +60,7 @@ function getWeather(lat, long) {
     })
     .then(function (data) {
       //console.log();
-      currentDate.textContent = moment().format("dddd, MMMM Do, LT");
+
       currentWeather.textContent = data.current.weather[0].description;
       currentTemp.textContent = "Temperature: " + data.current.temp + " F";
       currentWind.textContent =
@@ -62,4 +72,17 @@ function getWeather(lat, long) {
       currentHumidity.textContent = "Humidity: " + data.current.humidity + " %";
       currentUV.textContent = "UV Index: " + data.current.uvi;
     });
+}
+function updateLocalStorage(search) {
+  searchItems.push(search);
+  localStorage.setItem("previousSearch", JSON.stringify(searchItems));
+}
+function renderPreviousSearch() {
+  displayRecentSearch.innerHTML = "";
+  for (var i = 0; i < searchItems.length; i++) {
+    var searchItem = searchItems[i];
+    var previousSearch = document.createElement("li");
+    previousSearch.textContent = searchItem;
+    displayRecentSearch.append(previousSearch);
+  }
 }
